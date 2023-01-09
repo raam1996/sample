20c2608697222b355d680e07f4d31089bb5466ee
raam1996 <mukeshraam1996@gmail.com>
2023-01-09 15:17:48 +0530

    first commit

---
+++ src/org/mukeshraam/Boxer.java
@@ -0,0 +1,20 @@
+package org.mukeshraam;
+
+public class Boxer {
+	public int a;
+	private void savings() {
+		// TODO Auto-generated method stub
+		int a = 10;
+		System.out.println(a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Boxer aa = new Boxer();
+		System.out.println(aa.a);
+		aa.savings();
+		System.out.println(aa.a);
+
+	}
+
+}
